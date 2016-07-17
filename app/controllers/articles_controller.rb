29b260560468d5a06e5c53d372e4b7c85721e4d8
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-07-17 16:18:27 +0300

    @article.comments.all -> @article.comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article=Article.find(params[:id])
-    @comments=@article.comments.all
+    @comments=@article.comments
   end
 
  	def create
