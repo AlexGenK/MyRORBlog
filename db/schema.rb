b6c597e805db83cd3c9b0f025d041f245f9e4918
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-09-27 15:40:56 +0300

    add author column in the article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805203401) do
+ActiveRecord::Schema.define(version: 20160927121332) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "author",     default: "Alex"
   end
 
   create_table "comments", force: :cascade do |t|
