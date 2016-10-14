8599806b6ba0d13f8ffc28db1046ce4d3ad32e6b
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-10-14 22:49:53 +0300

    add specs for the length validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,6 +7,7 @@ describe Comment do
   describe "validation" do  
     it { should validate_presence_of(:author) }
     it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_most(4000) }
   end
   
   # тестируем ассоциции с моделью Article
