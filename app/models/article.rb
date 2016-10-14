a866eb256e96a4a28f978ce767b3d780c1407754
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-10-14 22:44:31 +0300

    add validation of length in Article and Comment models

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@
 
 class Article < ActiveRecord::Base
   validates :title, :text, presence: true
+  validates :title, length: { maximum: 140 }
+  validates :text, length: { maximum: 4000 }
   # статья может иметь много комментариев
   has_many :comments
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@
 
 class Comment < ActiveRecord::Base
   validates :author, :body, presence: true
+  validates :body, length: { maximum: 4000 }
   # комментарий привязан к одной статье
   belongs_to :article
 end
