4c5abbf4902992025acf27560f271cbca4eb4ea4
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-10-30 18:13:35 +0200

    add account creation test

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -14,4 +14,14 @@ feature "Account Creation" do
     expect(page).to have_content 'Registration'
   end
 
+  scenario "allow gest to create acount" do
+    visit new_user_registration_path
+    fill_in :user_username, :with => "TestUser"
+    fill_in :user_email, :with => "test@mail.ts"
+    fill_in :user_password, :with => "123456"
+    fill_in :user_password_confirmation, :with => "123456"
+    click_button "Sign up"
+    expect(page).to have_content I18n.t 'devise.registrations.signed_up'
+  end
+
 end
